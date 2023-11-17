8d02ceb7ca355eaa6b91d29ccd5bed8cca6a130e
asdErikasd <tikk219@gmail.com>
2023-11-17 19:52:52 +0200

    Lukujen lisaaminen

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-        String tekija = "Erik Riibe";
+        String tekija = "Erik Riibe"; 
+        int luku1 = 5; 
+        int luku2 = 2; 
+        int tulo; 
 		
+        // System.out.println(luku1); 
+        
 		System.out.println("Hei, olen Tulostin-ohjelma"); 
         System.out.println("Ohjelman tekijä: " + tekija); 
+        
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2); 
+        
+        tulo = luku1 * luku2;
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo); 
 	}
 
 }
